b4b6b7d1714dd5473b9fc367cd47680e0714e167
zhaoyunlong <zhaoyunlong@polarico.com>
2023-10-07 15:01:46 +0800

    init: 布局1

---
+++ src/components/app-set/index.vue
@@ -0,0 +1,114 @@
+<template>
+  <el-drawer v-model="drawer" :size="400" title="项目配置">
+    <el-divider>
+      <span>主题</span>
+    </el-divider>
+    <el-switch />
+    <el-divider>
+      <span>布局</span>
+    </el-divider>
+    <div class="layout-btns">
+      <div
+        :class="['btn', 'btn-classic', layoutActive === 'classic' ? 'active' : '']"
+        @click="setLayout('classic')"
+      ></div>
+      <div
+        :class="['btn', 'btn-topLeft', layoutActive === 'topLeft' ? 'active' : '']"
+        @click="setLayout('topLeft')"
+      ></div>
+      <div
+        :class="['btn', 'btn-top', layoutActive === 'top' ? 'active' : '']"
+        @click="setLayout('top')"
+      ></div>
+    </div>
+  </el-drawer>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const drawer = ref(false)
+function openSetDrawer() {
+  drawer.value = true
+}
+// 布局
+const layoutActive = ref('classic')
+function setLayout(selectLayout) {
+  layoutActive.value = selectLayout
+}
+defineExpose({
+  openSetDrawer
+})
+</script>
+
+<style lang="less" scoped>
+.layout-btns {
+  padding: 0 40px;
+  display: flex;
+  justify-content: space-around;
+  .btn {
+    width: 60px;
+    height: 60px;
+    background-color: #ddd;
+    border-radius: 4px;
+    border: 2px solid #ddd;
+    position: relative;
+    cursor: pointer;
+  }
+  .active {
+    border: 2px solid blue;
+  }
+  .btn-classic::before {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 1;
+    width: 33%;
+    height: 100%;
+    background-color: #273352;
+    border-radius: 4px 0 0 4px;
+    content: '';
+  }
+  .btn-classic::after {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 25%;
+    background-color: #fff;
+    border-radius: 4px 4px 0 4px;
+    content: '';
+  }
+  .btn-topLeft::before {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 33%;
+    height: 100%;
+    background-color: #fff;
+    border-radius: 4px 0 0 4px;
+    content: '';
+  }
+  .btn-topLeft::after {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 25%;
+    z-index: 1;
+    background-color: #273352;
+    border-radius: 4px 4px 0 0;
+    content: '';
+  }
+  .btn-top::after {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 25%;
+    z-index: 1;
+    background-color: #273352;
+    border-radius: 4px 4px 0 0;
+    content: '';
+  }
+}
+</style>
