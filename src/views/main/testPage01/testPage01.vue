4d1a62e14badfd973cf4e4f752ba33b1dcb44d95
zhaoyunlong <zhaoyunlong@polarico.com>
2023-10-20 09:40:36 +0800

    init: 主题色1

--- src/views/main/testPage01/testPage01.vue
+++ src/views/main/testPage01/testPage01.vue
@@ -1,11 +1,16 @@
 <template>
   <div>测试页1</div>
   <el-input v-model="input" placeholder="Please input" />
+  <el-button type="primary" @click="toggleDark()">改变主题色{{ isDark }}</el-button>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 const input = ref('')
+
+import { useDark, useToggle } from '@vueuse/core'
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
 </script>
 
 <style lang="scss" scoped></style>
